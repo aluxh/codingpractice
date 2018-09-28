1adad0bbdf04f64ff4aadd5663839a0731a30709
Alex Ho <alex.hochunsiong@gmail.com>
2018-09-28 16:54:44 +0800

    edit remove-space-from-string.js

--- remove-space-from-string.js
+++ remove-space-from-string.js
@@ -1,20 +1,21 @@
 /*
-Remove Spaces from a String</title>
-
+Sololearn - Code Challenge - Remove Spaces from a String
+---------------------------------------------------------
 Given a string as input, output it without spaces.
 For example:
     * string = "ab c  d e fgh i  j kl mn  opqr  stuvwxyz"
     * result = "abcdefghijklmnopqrstuvwxyz"
 */
-
-let input = "ab c  d e fgh i  j kl mn  opqr  stuvwxyz";
-
 const removeSpaces = (text) => {
     let output = "";
     for (let i of text) {
-        if (i != " ") output += i;
+        if (i == " ") continue;
+        output += i;
     }
     return output;
 }
+module.exports = removeSpaces;
 
-console.log(removeSpaces(input));
+// let input = "ab c  d e fgh i  j kl mn  opqr  stuvwxyz"
+// console.log(removeSpaces(input));
+// console.log(input.replace(/\s/g, ""));
---
+++ test.js
@@ -0,0 +1,3 @@
+const removeSpaces = require('./remove-space-from-string.js');
+
+console.log(removeSpaces("ab c  d e fgh i  j kl mn  opqr  stuvwxyz"));
