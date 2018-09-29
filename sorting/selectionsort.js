40bb8a9947993370a9a49953c268f414242f692f
Alex Ho <alex.hochunsiong@gmail.com>
2018-09-29 20:56:56 +0800

    improve the value swapping using destructuring

--- sorting/selectionsort.js
+++ sorting/selectionsort.js
@@ -5,9 +5,7 @@ function selectionSort(input) {
         let pivot = i; // Take 1 number (index) at a time to compare with the rest.
         for (let j = pivot+1; j < input.length; j++) { //take the number after it to compare.
             if (input[j] < input[pivot]) { //Take the number and compare with the rest of number. Swap when it is smaller.
-                let t = input[pivot];
-                input[pivot] = input[j];
-                input[j] = t;                
+                [input[j], input[pivot]] = [input[pivot], input[j]];
             }         
         }
         counter++;
