761203a42fc2c40956d72ef83e97a2273c14a58b
Alex Ho <alex.hochunsiong@gmail.com>
2018-09-29 15:13:06 +0800

    create module for insertion sort

--- sorting/insertionsort.js
+++ sorting/insertionsort.js
@@ -1,18 +1,17 @@
-let input = [3,1,5,2,9,8,6,5,4,5,6]; //6 times
-//let input = [1,2,3,4,5,6,7,8,9]; // 1 times
-//let input = [1,3,2,4,5,6,7,8,9]; // 2 times
-console.log(`Your input is ${input}`);
-let counter = 0;
-
-for (let i = 1; i < input.length; i++) {
-    for (let j = i; j > 0 && (input[j] < input[j-1]); j--) {
-    //if the next number in the list is bigger than the last number, it should just stay as is in position. Then take the next number.
-        if (input[j] < input[j-1]) {
-            [input[j], input[j-1]] = [input[j-1], input[j]];
+function insertionSort(input) {
+    let counter = 0;
+    for (let i = 1; i < input.length; i++) {
+        //console.log(input);
+        for (let j = i; j > 0 && (input[j] < input[j-1]); j--) {
+        /* If the next number in the list is bigger than the previous number, 
+           it should just stay as is in position. Then take the next number. */
+            if (input[j] < input[j-1]) {
+                [input[j], input[j-1]] = [input[j-1], input[j]];
+            }            
         }
         counter++;
     }
+    return [input , counter];
 }
 
-console.log(`output is ${input}`);
-console.log(`counter is ${counter}`);
+module.exports = insertionSort;
--- sorting/test.js
+++ sorting/test.js
@@ -1,5 +1,6 @@
 const bubbleSort = require('./bubblesort');
 const selectionSort = require('./selectionsort');
+const insertionSort = require('./insertionsort')
 
 const input = [3,1,5,2,9,8,6,5,4,5,6]; //6 times
 console.log(`Test input: ${input}\n`);
@@ -11,3 +12,7 @@ console.log(`Number of loops bubble sort took: ${by}\n`);
 const [sx, sy] = selectionSort(input);
 console.log(`Selection Sorted output: ${sx}`);
 console.log(`Number of loops selection sort took: ${sy}\n`);
+
+const [ix, iy] = insertionSort(input);
+console.log(`Insertion Sorted output: ${ix}`);
+console.log(`Number of loops insertion sort took: ${iy}\n`);
