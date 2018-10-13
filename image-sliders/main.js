315e28599794f7885614ebd95a1203ab1285f47d
Alex Ho <alex.hochunsiong@gmail.com>
2018-10-13 09:35:12 +0800

    use addEventListener and reduce to one simple function to move image left and right

--- image-sliders/main.js
+++ image-sliders/main.js
@@ -1,10 +1,3 @@
-/**
- * Building an Image Slider in the Javascript course, using all the lesson learnt.
- * 1. Load all the URLs for the images
- * 2. Initialized an index for manipulating the image to show
- * 3. Add events handling on Prev and Next button
- */
-
 window.onload = function() {
     let index = 0;
     let images = ["http://www.sololearn.com/uploads/slider/1.jpg", "http://www.sololearn.com/uploads/slider/2.jpg", "http://www.sololearn.com/uploads/slider/3.jpg"];
@@ -15,28 +8,16 @@ window.onload = function() {
 
     slider.src = images[index];
 
-    prev.onclick = () => {
-        // (1) Decrease index by 1 
-        // (2) Test whether decreased index go below 0, and if it does, reset it to the index of last image 
-        // (3) assign the image to the img src.
-        index -= 1;
+    prev.addEventListener('click', () => move(-1));
+    next.addEventListener('click', () => move(1));
+
+    function move(direction) {
+        index += direction;
         if (index < 0) {
             index = images.length - 1;
-        }
-        slider.src = images[index];
-    }
-
-    next.onclick = () => {
-        // (1) Increase index by 1 
-        // (2) Test whether increased index go beyond the last index, and if it does, reset it to the 1st image 
-        // (3) assign the image to the img src.
-        index += 1;
-        if (index > (images.length - 1)) {
+        } else if (index > images.length - 1) {
             index = 0;
         }
         slider.src = images[index];
     }
 }
-
-
-
