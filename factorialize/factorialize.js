bde3f4c1bbc58c4438e0b1869a4c543e9259ea9b
Alex Ho <alex.hochunsiong@gmail.com>
2018-10-13 10:33:46 +0800

    change the function to using recursion

--- factorialize/factorialize.js
+++ factorialize/factorialize.js
@@ -10,11 +10,11 @@
  */
 
 function factorialize(num) {
-    let result = 1;
-    for (let i = num; i > 0; i--) {
-        result *= i;
-    }
-    return result;
-  }
+    if (num == 0) return 1;
+    return num * factorialize(num - 1);
+}
   
-console.log(factorialize(5));
+console.log(factorialize(5)); // 120
+console.log(factorialize(10)); // 3628800
+console.log(factorialize(20)); // 2432902008176640000
+console.log(factorialize(0)); // 1
