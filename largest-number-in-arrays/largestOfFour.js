6e5a4158a03d8a2ab8068ceda4967bf8e7fd7a9a
aluxh <alex.ho@autodesk.com>
2018-11-23 15:14:28 +0800

    Add confirm-ending from freecodecamp

--- largest-number-in-arrays/largestOfFour.js
+++ largest-number-in-arrays/largestOfFour.js
@@ -9,6 +9,13 @@ function largestOfFour(arr) {
     // }
     return arr;
 }
+
+/*
+function largestOfFour(arr) {
+    arr = arr.map((x) => Math.max(...x));
+    return arr;
+}
+*/
   
 console.log(largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]])); // [27, 5, 39, 1001]
 console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]])); // [9, 35, 97, 1000000]
