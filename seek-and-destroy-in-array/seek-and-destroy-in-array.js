e4a0e81ec907e322aa31798abe3733bee356ddf2
Alex Ho <alex.hochunsiong@gmail.com>
2018-12-08 08:53:48 +0800

    add seek-and-destroy

--- seek-and-destroy-in-array/seek-and-destroy-in-array.js
+++ seek-and-destroy-in-array/seek-and-destroy-in-array.js
@@ -10,18 +10,16 @@ You have to use the arguments object.
 
 function destroyer(arr) {
     // Remove all the values
-    
+    for (let i = 1; i < arguments.length; i++) {
+      arr = arr.filter(x => x !== arguments[i]);
+    }
     return arr;
-}
+  }
   
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
-
-
-/*
+// Test
 console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3)); // should return [1, 1].
 console.log(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3)); // should return [1, 5, 1].
 console.log(destroyer([3, 5, 1, 2, 2], 2, 3, 5)); // should return [1].
 console.log(destroyer([2, 3, 2, 3], 2, 3)); // should return [].
 console.log(destroyer(["tree", "hamburger", 53], "tree", 53)); // should return ["hamburger"].
 console.log(destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan")); // should return [12,92,65].
-*/
